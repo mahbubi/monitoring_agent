38c9e71f63c19ba37b9612b1eedcffb384087c8f
hamdani <hamdani@boer.co.id>
2022-10-20 10:39:15 +0700

    edit bug

--- Installer.sh
+++ Installer.sh
@@ -10,14 +10,14 @@ sudo systemctl daemon-reload && sudo systemctl enable --now node_exporter.servic
 sudo systemctl status node_exporter.service
 echo "################ Test Curl Node Exporter ################"
 curl localhost:9100
-echo "################ Instal Package Loki ################"
+echo "################ Instal Package Promtail ################"
 curl -s https://api.github.com/repos/grafana/loki/releases/latest | grep browser_download_url |  cut -d '"' -f 4 | grep promtail-linux-amd64.zip | wget -i -  
 sudo apt install unzip && unzip promtail-linux-amd64.zip 
 sudo mv promtail-linux-amd64 promtail
 sudo cp promtail /usr/local/bin
 sudo mkdir -p /etc/promtail
+echo "################ Move SystemD &Restart Service promtail.service ################"
 mv promtail.yaml /etc/promtail/promtail.yaml
 mv promtail.service /etc/systemd/system/promtail.service
-echo "################ Restart Service Node_exporter.service ################"
-sudo systemctl daemon-reload && sudo systemctl enable --now node_exporter.service 
-sudo systemctl status node_exporter.service
+sudo systemctl daemon-reload && sudo systemctl enable --now promtail.service
+sudo systemctl status promtail.service
