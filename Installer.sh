e215c9305694fde8355c09785b367258d0721098
hamdani <hamdani@boer.co.id>
2022-10-20 11:47:49 +0700

    add some text

--- Installer.sh
+++ Installer.sh
@@ -4,21 +4,26 @@ sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/n
 sudo tar xvf node_exporter-1.3.1.linux-amd64.tar.gz 
 sudo mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/ 
 sudo useradd -rs /bin/false node_exporter
+echo "Instal Package node_exporte DONE"
 echo "################ Move SystemD & Restart Service Node_exporter.service ################"
 mv node_exporter.service /etc/systemd/system/node_exporter.service
 sudo systemctl daemon-reload && sudo systemctl enable --now node_exporter.service 
+echo "Move SystemD & Restart Service Node_exporter.service DONE"
 echo "################ Test Curl Node Exporter ################"
 curl localhost:9100
+echo "Test Curl Node Exporter Done"
 echo "################ Instal Package Promtail ################"
 curl -s https://api.github.com/repos/grafana/loki/releases/latest | grep browser_download_url |  cut -d '"' -f 4 | grep promtail-linux-amd64.zip | wget -i -  
 sudo apt install unzip && unzip promtail-linux-amd64.zip 
 sudo mv promtail-linux-amd64 promtail
 sudo cp promtail /usr/local/bin
 sudo mkdir -p /etc/promtail
+echo "Instal Package Promtail Done"
 echo "################ Move SystemD &Restart Service promtail.service ################"
 mv promtail.yaml /etc/promtail/promtail.yaml
 mv promtail.service /etc/systemd/system/promtail.service
 sudo systemctl daemon-reload && sudo systemctl enable --now promtail.service
+echo "Move SystemD &Restart Service promtail.service Done"
 echo "################ Check Service promtail & node_exporter ################"
 STATUS1="$(systemctl is-active node_exporter.service)"
 STATUS2="$(systemctl is-active promtail.service)"
